ca1222113df4d188755e7e67356ca2735d307699
Beycan <bbeycanov@gmail.com>
2020-02-19 13:04:06 +0400

    Update example.php

--- example.php
+++ example.php
@@ -21,6 +21,6 @@
   $token = $paytr->token;
 ?>
 
-  <script src="https://www.paytr.com/js/iframeResizer.min.js"></script>
-  <iframe src="https://www.paytr.com/odeme/guvenli/<?php echo $token;?>" id="paytriframe" frameborder="0" scrolling="no" style="width: 100%;"></iframe>
-	<script>iFrameResize({},'#paytriframe');</script>
+<script src="https://www.paytr.com/js/iframeResizer.min.js"></script>
+<iframe src="https://www.paytr.com/odeme/guvenli/<?php echo $token;?>" id="paytriframe" frameborder="0" scrolling="no" style="width: 100%;"></iframe>
+<script>iFrameResize({},'#paytriframe');</script>
